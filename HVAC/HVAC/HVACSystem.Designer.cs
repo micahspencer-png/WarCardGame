bcebc8e11eb1326e53ded211e2c05ffd98c088cf
micahspencer-png <micahspencer@isu.edu>
2026-04-20 11:18:42 -0600

    added a toolstrip and preset the logic for the QY@ board

---
+++ HVAC/HVAC/HVACSystem.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace HVACSmartHomeController
+{
+    partial class HVACSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HVACSystem));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.PortComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.ConnectButton = new System.Windows.Forms.ToolStripButton();
+            this.ExitButton = new System.Windows.Forms.ToolStripButton();
+            this.SaveButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PortComboBox,
+            this.ConnectButton,
+            this.ExitButton,
+            this.SaveButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 557);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(1007, 35);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(121, 35);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ConnectButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectButton.Image = ((System.Drawing.Image)(resources.GetObject("ConnectButton.Image")));
+            this.ConnectButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(93, 32);
+            this.ConnectButton.Text = "&Connect";
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ExitButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Image = ((System.Drawing.Image)(resources.GetObject("ExitButton.Image")));
+            this.ExitButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(52, 32);
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.SaveButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Image = ((System.Drawing.Image)(resources.GetObject("SaveButton.Image")));
+            this.SaveButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(60, 32);
+            this.SaveButton.Text = "&Save";
+            // 
+            // HVACSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 592);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "HVACSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HVAC Control";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox PortComboBox;
+        private System.Windows.Forms.ToolStripButton ConnectButton;
+        private System.Windows.Forms.ToolStripButton ExitButton;
+        private System.Windows.Forms.ToolStripButton SaveButton;
+    }
+}
+
---
+++ HVAC/HVAC/HVACSystem.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HVACSmartHomeController
+{
+    public partial class HVACSystem : Form
+    {
+        public HVACSystem()
+        {
+            InitializeComponent();
+        }
+        //ISU Color Palette
+        Color GrowlGreyLight = Color.FromArgb(230, 231, 232);
+        Color GrowlGreyMed = Color.FromArgb(167, 167, 167);
+        Color GrowlGrey = Color.FromArgb(130, 130, 130);
+        Color Roarange = Color.FromArgb(244, 121, 32);
+        Color RoarangeL = Color.FromArgb(246, 146, 64);
+        Color BengalBlack = Color.FromArgb(0, 0, 0);
+        //Program Logic------------------------------------------------------------------------------------------------------------------
+
+        void SerialConnect(string name)
+        {
+            serialPort1.Close();
+            serialPort1.BaudRate = 115200;
+            serialPort1.PortName = name;
+            serialPort1.Parity = System.IO.Ports.Parity.None;
+            //serialPort1.StopBits = System.IO.Ports.StopBits.None;
+            try
+            {
+                serialPort1.Open();
+                if (serialPort1.IsOpen)
+                {
+                    //MessageBox.Show($"{serialPort1.PortName} Connected Successfully");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            //ConnectButton.Enabled = false;
+        }
+        byte[] SendData(byte[] data)
+        {
+            byte[] buffer = new byte[0];
+            try
+            {
+
+                if (serialPort1.IsOpen)
+                {
+                    //flush old bytes from buffers
+                    serialPort1.DiscardInBuffer();
+                    serialPort1.DiscardOutBuffer();
+
+                    //send command
+                    serialPort1.Write(data, 0, data.Length);
+
+                    //wait for response
+                    Thread.Sleep(50);
+                    //make the array the size of the input buffer
+                    buffer = new byte[serialPort1.BytesToRead];
+                    //actually read the input buffer
+                    serialPort1.Read(buffer, 0, buffer.Length);
+                }
+                return buffer;
+            }
+            catch
+            {
+                return buffer;
+            }
+        }
+
+        void GetQYAtBoards()
+        {
+            PortComboBox.Text = "";
+            PortComboBox.Items.Clear();
+            string[] names = SerialPort.GetPortNames();
+            foreach (string name in names)
+            {
+                SerialConnect(name);
+                if (IsQYAtBoardCheck())
+                {
+                    PortComboBox.Items.Add(name);
+                }
+            }
+            if (PortComboBox.Items.Count > 0)
+            {
+                PortComboBox.SelectedIndex = 0;
+            }
+            serialPort1.Close();
+        }
+
+        bool IsQYAtBoardCheck()
+        {
+            bool QYAt = false;
+            byte[] QyAtSettings = { 0xf0 };
+            byte[] ReadBuffer = SendData(QyAtSettings);
+            if (ReadBuffer.Length == 64 && ReadBuffer[58] == 81 && ReadBuffer[59] == 121 && ReadBuffer[60] == 64)
+            {
+                //MessageBox.Show($"QY@ Board Connected  to {serialPort1.PortName}");
+                QYAt = true;
+            }
+            return QYAt;
+        }
+        int ReadAnalogOne()
+        {
+            byte[] data = { 0x51 };
+            byte[] response = SendData(data);
+            if (response.Length == 2)
+            {
+                return (response[0] << 2) + (response[1] >> 6);
+            }
+            return -1;
+        }
+        int ReadAnalogTwo()
+        {
+            byte[] data = { 0x52 };
+            byte[] response = SendData(data);
+            if (response.Length == 2)
+            {
+                return (response[0] << 2) + (response[1] >> 6);
+            }
+            return -1;
+        }
+        int WriteDigitalOut(byte x)
+        {
+            byte[] data = { 0x20, x };
+            byte[] response = SendData(data);
+            if (response.Length == 2)
+            {
+                return (response[0] << 2) + (response[1] >> 6);
+            }
+            return -1;
+        }
+        int ReadDigitalIn()
+        {
+            byte[] data = { 0x30 };
+            byte[] response = SendData(data);
+            if (response.Length == 2)
+            {
+                return (response[0] << 2) + (response[1] >> 6);
+            }
+            return -1;
+        }
+
+        //Event Handlers-----------------------------------------------------------------------------------------------------------------
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
