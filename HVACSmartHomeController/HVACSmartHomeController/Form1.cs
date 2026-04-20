7e41ebfa5913f04c65de40f99030c26bf3a0f849
micahspencer-png <micahspencer@isu.edu>
2026-04-20 10:40:17 -0600

    added color palete to file

---
+++ HVACSmartHomeController/HVACSmartHomeController/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HVACSmartHomeController
+{
+    public partial class HVACSystem : Form
+    {
+        public HVACSystem()
+        {
+            InitializeComponent();
+        }
+        //ISU Color Palette
+        Color GrowlGreyLight = Color.FromArgb(230, 231, 232);
+        Color GrowlGreyMed = Color.FromArgb(167, 167, 167);
+        Color GrowlGrey = Color.FromArgb(130, 130, 130);
+        Color Roarange = Color.FromArgb(244, 121, 32);
+        Color RoarangeL = Color.FromArgb(246, 146, 64);
+        Color BengalBlack = Color.FromArgb(0, 0, 0);
+
+
+    }
+}
